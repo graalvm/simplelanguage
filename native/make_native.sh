6662edaf1688030629faf7f9d2b4199a90f0e09e
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-08-03 09:09:29 +0200

    RC5 update.

--- native/make_native.sh
+++ native/make_native.sh
@@ -4,6 +4,6 @@ if [[ $SL_BUILD_NATIVE == "false" ]]; then
     exit 0
 fi
 $JAVA_HOME/bin/native-image --tool:truffle -H:MaxRuntimeCompileMethods=1200 \
-    -cp ../language/target/simplelanguage.jar:../launcher/target/launcher-1.0.0-rc4-SNAPSHOT.jar \
+    -cp ../language/target/simplelanguage.jar:../launcher/target/launcher-1.0.0-rc5-SNAPSHOT.jar \
     com.oracle.truffle.sl.launcher.SLMain \
     slnative
