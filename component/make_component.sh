7c32d5a982cbffbcae1fe5f8bcd0561345e22eb4
Christian Humer <christian.humer@oracle.com>
2018-08-16 18:13:32 +0200

    Update make_component.sh to RC5.

--- component/make_component.sh
+++ component/make_component.sh
@@ -19,8 +19,8 @@ MANIFEST="$COMPONENT_DIR/META-INF/MANIFEST.MF"
 touch "$MANIFEST"
 echo "Bundle-Name: Simple Language" >> "$MANIFEST"
 echo "Bundle-Symbolic-Name: com.oracle.truffle.sl" >> "$MANIFEST"
-echo "Bundle-Version: 1.0.0-rc2" >> "$MANIFEST"
-echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=1.0.0-rc2)(os_arch=amd64))"' >> "$MANIFEST"
+echo "Bundle-Version: 1.0.0-rc5" >> "$MANIFEST"
+echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=1.0.0-rc5)(os_arch=amd64))"' >> "$MANIFEST"
 echo "x-GraalVM-Polyglot-Part: True" >> "$MANIFEST"
 
 cd $COMPONENT_DIR
