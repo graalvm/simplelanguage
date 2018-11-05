74791466649bddbeea6e224fd03384028a360ab5
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-11-05 15:33:08 +0100

    Fix clean component script.

--- component/clean_component.sh
+++ component/clean_component.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[-f sl-component.jar] && rm sl-component.jar || echo "sl-component.jar not found"
+[[ -f sl-component.jar ]] && rm sl-component.jar || echo "sl-component.jar not found"
