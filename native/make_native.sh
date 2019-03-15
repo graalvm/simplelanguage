a32bd73d226ff8a08418dd236146ac4e9293106b
Boris Spasojevic <boris.spasojevic@oracle.com>
2019-03-15 14:09:59 +0100

    Prevent globbing and word splitting when building native image.

--- native/make_native.sh
+++ native/make_native.sh
@@ -3,7 +3,7 @@ if [[ $SL_BUILD_NATIVE == "false" ]]; then
     echo "Skipping the native image build because SL_BUILD_NATIVE is set to false."
     exit 0
 fi
-$JAVA_HOME/bin/native-image --tool:truffle -H:MaxRuntimeCompileMethods=1200 \
-    -cp ../language/target/simplelanguage.jar:../launcher/target/launcher-1.0.0-rc13-SNAPSHOT.jar \
+"$JAVA_HOME"/bin/native-image --tool:truffle -H:MaxRuntimeCompileMethods=1200 \
+    -cp ../language/target/simplelanguage.jar:../launcher/target/launcher-1.0.0-rc10-SNAPSHOT.jar \
     com.oracle.truffle.sl.launcher.SLMain \
     slnative
